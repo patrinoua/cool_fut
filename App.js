df20bc0b465fcf7d870135b4afcb386f49df95ef
Mona Lisa <patrinoua@gmail.com>
2020-03-20 19:15:20 +0100

    name and lastname fields

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading} placeholder="Name">
+        Name
+      </Text>
+      <TextInput style={styles.input} placeholder="Name" />
+      <Text style={styles.heading}>Last name</Text>
+      <TextInput style={styles.input} placeholder="Last name" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    paddingLeft: 40,
+    paddingRight: 40
   },
-});
+  heading: {
+    fontSize: 24,
+    marginBottom: 10,
+    marginTop: 25,
+    color: 'darkgrey'
+  },
+  input: {
+    alignSelf: 'stretch',
+    borderBottomColor: '#e7e7e7',
+    borderBottomWidth: 1,
+    height: 30
+  }
+})
---
+++ elements.js
@@ -0,0 +1,11 @@
+// import 'styled' from 'styled-components'
+
+// export const SimpleInputField = styled.TextInput`
+//   align-self: stretch;
+//   height: 30px;
+//   margin-bottom: 20px;
+//   margin-left: 20px;
+//   margin-right: 20px;
+//   border-bottom-color: #e7e7e7;
+//   border-bottom-width: 1px;
+// `
